5a0c685cf128abb677d51b92a846c19535a45991
Aman Choudhary <Aman.Choudhary@philips.com>
2024-04-04 20:26:30 +0530

    4apr24

--- cwh-js/video1.js
+++ cwh-js/video1.js
@@ -1,17 +1,25 @@
-// ECMAScript and JavaScript are often used interchangeably, but there is a subtle difference between the two. ECMAScript is a specification that defines the syntax, semantics, and behavior of a scripting language. JavaScript is an implementation of the ECMAScript specification.
+// ECMAScript and JavaScript are often used interchangeably, but there is a subtle difference 
+//between the two. ECMAScript is a specification that defines the syntax, semantics, and 
+//behavior of a scripting language. JavaScript is an implementation of the ECMAScript
+// specification.
 
-// JavaScript is the most widely used implementation of ECMAScript and is used extensively for web development. Other implementations of ECMAScript include JScript and ActionScript.
+// JavaScript is the most widely used implementation of ECMAScript and is used extensively 
+//for web development. Other implementations of ECMAScript include JScript and ActionScript.
 
-// JavaScript has evolved over the years to include new features that were added to the ECMAScript specification. For example, ECMAScript 6 (ES6) introduced new features such as let and const for variable declaration, arrow functions, and classes. These features were later implemented in JavaScript engines in web browsers and Node.js.
+// JavaScript has evolved over the years to include new features that were added to the
+// ECMAScript specification. For example, ECMAScript 6 (ES6) introduced new features such as 
+//let and const for variable declaration, arrow functions, and classes. These features were 
+//later implemented in JavaScript engines in web browsers and Node.js.
 
-// In summary, ECMAScript is a specification that defines the scripting language while JavaScript is an implementation of the ECMAScript specification.
+// In summary, ECMAScript is a specification that defines the scripting language while 
+//JavaScript is an implementation of the ECMAScript specification.
 
 
 // we can also run js online on repli 
 
 // js is programming lang used to make interactive web pages
 
- 
+
 
 let a;
 console.log(a);
--- cwh-js/video2_variable.js
+++ cwh-js/video2_variable.js
@@ -1,17 +1,23 @@
 // // js is dynamic type
 
-// There are two main differences between dynamic typing and static typing that you should be aware of when writing transformation scripts.
+// There are two main differences between dynamic typing and static typing that you should be
+// aware of when writing transformation scripts.
 
-// First, dynamically-typed languages perform type checking at runtime, while statically typed languages perform type checking at compile time. This means that scripts written in dynamically-typed languages (like Groovy) can compile even if they contain errors that will prevent the script from running properly (if at all). If a script written in a statically-typed language (such as Java) contains errors, it will fail to compile until the errors have been fixed.
+// First, dynamically-typed languages perform type checking at runtime, while statically 
+//typed languages perform type checking at compile time. This means that scripts written in
+// dynamically-typed languages (like Groovy) can compile even if they contain errors that
+// will prevent the script from running properly (if at all). If a script written in a 
+//statically-typed language (such as Java) contains errors, it will fail to compile until
+// the errors have been fixed.
 
 // Second, statically-typed languages require you to declare the data types of your variables before you use them, while dynamically-typed languages do not
 
 
 let a;
-a=55;
+a = 55;
 //const b;// error bec we have to initialized const along declaration
 let b;
-c=555;
+c = 555;
 
 console.log(typeof a);
 console.log(typeof b);
